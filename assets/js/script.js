b19703386ea8ee73cf39d7e78d029faa792a8767
thossain30 <54952959+thossain30@users.noreply.github.com>
2023-02-20 20:45:17 -0500

    Added logic to transition between start page and questions page and also fixed some timer bugs

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,7 @@
+// Start Page
+let startPage = document.querySelector(".start-page")
+// Page with questions and answers on it
+let questionPage = document.querySelector(".question");
 // Button to begin the quiz
 let startButton = document.querySelector(".start-button");
 // Tool to debug timer related events
@@ -5,11 +9,16 @@ let stopButton = document.querySelector(".stop");
 // Amount of time left
 let timeRemaining = document.querySelector(".start-timer");
 
-let timeLeft = parseInt(timeRemaining.textContent);
-function startTimer() {
+let timeLeft;
+function startQuiz() {
+    timeRemaining.textContent = 19;
+    timeRemaining.setAttribute("style", "color: black")
+    timeLeft = parseInt(timeRemaining.textContent);
+    startPage.classList.add("hide");
+    questionPage.classList.remove("hide");
     time = setInterval(() => {
         if (timeLeft <= 1) {
-            stopTimer();
+            stopQuiz();
         }
 
         timeLeft--;
@@ -25,12 +34,16 @@ function startTimer() {
 
 
 // Stops Timer and runs code for losing
-function stopTimer() {
+function stopQuiz() {
     clearInterval(time);
-    timeRemaining.textContent = 19 + " ";
-    timeRemaining.setAttribute("style", "color: black")
+    questionPage.classList.add("hide");
+    startPage.classList.remove("hide");
 }
 
-startButton.addEventListener("click", startTimer);
+function setNextQuestion() {
+
+}
+
+startButton.addEventListener("click", startQuiz);
 // Tool to debug timer related events
-stopButton.addEventListener("click", stopTimer)
+stopButton.addEventListener("click", stopQuiz)
