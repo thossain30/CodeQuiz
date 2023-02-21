f276de74d31191570aa81730709be94207a88a57
thossain30 <54952959+thossain30@users.noreply.github.com>
2023-02-20 20:05:33 -0500

    Added CSS and more elements

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,8 @@
+// Button to begin the quiz
 let startButton = document.querySelector(".start-button");
+// Tool to debug timer related events
 let stopButton = document.querySelector(".stop");
+// Amount of time left
 let timeRemaining = document.querySelector(".start-timer");
 
 let timeLeft = parseInt(timeRemaining.textContent);
@@ -8,12 +11,14 @@ function startTimer() {
         if (timeLeft <= 1) {
             stopTimer();
         }
+
         timeLeft--;
 
         // Turns time red when you have less than 10 seconds left
         if (timeLeft < 10) {
             timeRemaining.setAttribute("style", "color: red")
         }
+        // Sets time remaining text to new decremented number
         timeRemaining.textContent = timeLeft + " ";
     }, 1000);
 }
@@ -22,9 +27,10 @@ function startTimer() {
 // Stops Timer and runs code for losing
 function stopTimer() {
     clearInterval(time);
+    timeRemaining.textContent = 19 + " ";
+    timeRemaining.setAttribute("style", "color: black")
 }
 
 startButton.addEventListener("click", startTimer);
-
 // Tool to debug timer related events
 stopButton.addEventListener("click", stopTimer)
