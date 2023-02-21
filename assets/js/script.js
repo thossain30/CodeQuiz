262f791a188993fac399d920f28dc5b2b0b415f6
thossain30 <54952959+thossain30@users.noreply.github.com>
2023-02-21 02:22:33 -0500

    Finished implementation of scoring and added all the questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,6 +8,9 @@ let startButton = document.querySelector(".start-button");
 let stopButton = document.querySelector(".stop");
 // Amount of time left
 let timeRemaining = document.querySelector(".start-timer");
+// Player Score
+let score = document.querySelector(".score"); 
+let scoreNum = parseInt(score.textContent);
 
 let shuffledQuestions, currentQuestionIndex;
 let questionElement = document.querySelector(".question");
@@ -16,6 +19,8 @@ let answersElement = document.querySelector(".answers");
 let timeLeft;
 function startQuiz() {
     timeRemaining.textContent = 19;
+    score.textContent = 0;
+    score.setAttribute("style", "color: black");
     timeRemaining.setAttribute("style", "color: black")
     timeLeft = parseInt(timeRemaining.textContent);
     shuffledQuestions = questions.sort(() => Math.random() - .5);
@@ -32,7 +37,7 @@ function startQuiz() {
 
         // Turns time red when you have less than 10 seconds left
         if (timeLeft < 10) {
-            timeRemaining.setAttribute("style", "color: red")
+            timeRemaining.setAttribute("style", "color: red; font-size: 32px");
         }
         // Sets time remaining text to new decremented number
         timeRemaining.textContent = timeLeft + " ";
@@ -80,7 +85,15 @@ function resetState() {
 function selectAnswer(e) {
     const selectedButton = e.target;
     const correct = selectedButton.dataset.correct;
-    console.log("index: " + currentQuestionIndex);
+    if (correct) { 
+        scoreNum += 100;
+        score.textContent = scoreNum;
+        score.setAttribute("style", "color: green");
+    } else {
+        scoreNum -= 25;
+        score.textContent = scoreNum;
+        score.setAttribute("style", "color: red");
+    }
     if (currentQuestionIndex >= questions.length - 1) {
         stopQuiz();
     } else {
@@ -108,12 +121,12 @@ function clearStatus(element) {
 }
 let questions = [
     {
-        question: "What is 2 + 2?", 
+        question: "Which of these is NOT a primitive data type?", 
         answers: [
-            {text: "4", correct: true},
-            {text: "22", correct: false},
-            {text: "38", correct: false},
-            {text: "3", correct: false},
+            {text: "null", correct: false},
+            {text: "string", correct: false},
+            {text: "boolean", correct: false},
+            {text: "object", correct: true},
         ]
     },
     {
@@ -123,6 +136,78 @@ let questions = [
             {text: "script", correct: false},
             {text: "js", correct: true},
             {text: "jsx", correct: false},
+        ]
+    },
+    {
+        question: "Inside which HTML element do we put our JavaScript file?", 
+        answers: [
+            {text: "<javascript>", correct: false},
+            {text: "<script>", correct: true},
+            {text: "<js>", correct: false},
+            {text: "<scripting>", correct: false},
+        ]
+    },
+    {
+        question: "What is the correct syntax for referring to an external script called \"xxx.js\"?", 
+        answers: [
+            {text: "<script href=\"xxx.js\">", correct: false},
+            {text: "<script src=\"xxx.js\">", correct: true},
+            {text: "<script code=\"xxx.js\">", correct: false},
+            {text: "<script name='xxx.js'>", correct: false},
+        ]
+    },
+    {
+        question: "How do you write \"Hello World\" in an alert box?", 
+        answers: [
+            {text: "alertBox(\"Hello World\");", correct: false},
+            {text: "msgBox(\"Hello World\");", correct: false},
+            {text: "msg(\"Hello World\");", correct: false},
+            {text: "alert(\"Hello World\");", correct: true},
+        ]
+    },
+    {
+        question: "How do you write an IF statement in Javascript?", 
+        answers: [
+            {text: "if i == 5 then", correct: false},
+            {text: "if (i == 5)", correct: true},
+            {text: "if i = 5 then", correct: false},
+            {text: "if i = 5", correct: false},
+        ]
+    },
+    {
+        question: "How does a FOR loop start?", 
+        answers: [
+            {text: "for (let i = 0; i <= 5; i++)", correct: true},
+            {text: "for i = 1 to 5", correct: false},
+            {text: "for (i = 0; i <= 5)", correct: false},
+            {text: "for (i <= 5; i++)", correct: false},
+        ]
+    }, 
+    {
+        question: "What is the correct way to write a JavaScript array?", 
+        answers: [
+            {text: "var colors = [\"red\", \"green\", \"blue\"]", correct: true},
+            {text: "var colors = \"red\", \"green\", \"blue\"", correct: false},
+            {text: "var colors = 1 = (\"red\"), 2 = (\"green\"), 3 = (\"blue\")", correct: false},
+            {text: "var colors = (1:\"red\", 2:\"green\", 3:\"blue\")", correct: false},
+        ]
+    },
+    {
+        question: "How do you find the number with the highest value of x and y?", 
+        answers: [
+            {text: "top(x,y)", correct: false},
+            {text: "ceil(x,y)", correct: false},
+            {text: "Math.ceil(x,y)", correct: false},
+            {text: "Math.max(x,y)", correct: true},
+        ]
+    },
+    {
+        question: "Which operator is used to assign a value to a variable?", 
+        answers: [
+            {text: "*", correct: false},
+            {text: "X", correct: false},
+            {text: "=", correct: true},
+            {text: "-", correct: false},
         ]
     }
 ]
